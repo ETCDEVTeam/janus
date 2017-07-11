e8fb33f1e0aaccef15dbbf5f938dd4e82cc9ca46
ia <isaac.ardis@gmail.com>
2017-07-11 08:52:11 -0500

    problem: deploy script used only as proof-of-concept
    solution: rename it -example

--- deploy-example.sh
+++ deploy-example.sh
@@ -2,5 +2,4 @@
 
 echo "Deploying..."
 
-# TODO: replace me with the real stuff; bucket, object, file, key
 janus deploy -to "isaac-tests/janus/$(janus version -format %M.%m.x)" -files "*.zip" -key isaac-test-key.enc.json
