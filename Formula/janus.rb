3b3ba1feec831f91ce56d489f6a24915d3dbb993
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-11 09:01:41 -0500

    janus version v0.1.0

--- Formula/janus.rb
+++ Formula/janus.rb
@@ -1,9 +1,9 @@
 class Janus < Formula
   desc "Shared CI deployer + version syntax tool for ETCDEV"
   homepage ""
-  url "https://github.com/whilei/janus/releases/download/v0.1.1/janus_v0.1.1_Darwin_x86_64.tar.gz"
-  version "0.1.1"
-  sha256 "d7ea50ba89ce46ec2bd40c39f32a3f4b2d2989d9bc2b71c7e1f944aed5871e54"
+  url "https://github.com/whilei/janus/releases/download/v0.1.0/janus_v0.1.0_Darwin_x86_64.tar.gz"
+  version "0.1.0"
+  sha256 "23e46f30666571ca8f52d6663975ec76c55637ed42d712804d500b8240929fc9"
 
   def install
     bin.install "janus"
