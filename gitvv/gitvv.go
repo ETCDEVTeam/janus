343343877289361641c0c80740bbfb0f3d1df556
ia <isaac.ardis@gmail.com>
2017-07-22 11:03:17 -0500

    problem: refactor, add tests, and implement %B functionality
    solution: does that.
    
    Note: tests for CI are currently disabled since I haven't got
    the required submodule functionality working on CI yet.

--- gitvv/gitvv.go
+++ gitvv/gitvv.go
@@ -1,29 +1,23 @@
 package gitvv
 
 import (
-	"fmt"
+	"errors"
 	"log"
 	"os/exec"
 	"regexp"
-	"strings"
 	"strconv"
-	"errors"
+	"strings"
 )
 
 type gitDescription int
 
-const (
-	gitTag             gitDescription = iota // v3.5.0
-	gitHash                                  // bbb06b1 or gbbb06b1
-	gitFullDescription                       // v3.5.0-66-gbbb06b1
-)
-
 const defaultHashLength = 7
 
 var (
-	cacheDescription string
-	cacheCommitCount string
-	cacheHEADHash    string
+	cacheLastTagName            string
+	cacheCommitCountFromTagName string
+	cacheCommitCount            string
+	cacheHEADHash               string
 )
 
 func isHash(s string) bool {
@@ -39,51 +33,43 @@ func isHash(s string) bool {
 	return false
 }
 
-func getCommitCountFromDescription(s string) string {
-	switch getTypeofDescription(s) {
-	case gitTag:
-		return "0"
-	case gitFullDescription:
-		return strings.Split(s, "-")[1]
+// getTagOnHEADCommit gets the tag on the current commit, else
+// returns "" if no tag on current commit
+func getTagIfTagOnHEADCommit() (string, bool) {
+	//git describe --exact-match --abbrev=0
+	c, e := exec.Command("git", "describe", "--exact-match", "--abbrev=0").CombinedOutput()
+	if e != nil {
+		//log.Println(e)
+		return "", false
 	}
-	return getCommitCount()
+	tag := strings.TrimSpace(string(c))
+	if tag == "" {
+		return tag, false
+	}
+	return tag, true
 }
 
-// Assumes using semver format for tags, eg v3.5.0 or 3.4.0
-func getSemverFromDescription(s string) []string {
-	if getTypeofDescription(s) == gitHash {
-		return []string{"0", "0", "0"}
-	}
-	tag := strings.Split(s, "-")[0]
-	tag = strings.TrimPrefix(tag, "v")
-	return strings.Split(tag, ".")
-}
-
-// getTypeofDescription returns the kind of log/tag as iota const
-func getTypeofDescription(s string) gitDescription {
-	ss := strings.Split(s, "-")
-	if len(ss) == 1 {
-		if isHash(ss[0]) {
-			return gitHash
-		}
-		return gitTag
-	}
-	return gitFullDescription
-}
-
-func getCommitCount() string {
-	if cacheCommitCount != "" {
+func getCommitCountFrom(fromTag string) string {
+	if cacheCommitCount != "" && cacheCommitCountFromTagName == fromTag {
 		return cacheCommitCount
 	}
 
-	c, e := exec.Command("git", "rev-list", "HEAD", "--count").Output()
+	reference := "HEAD"
+	if fromTag != "" {
+		reference = fromTag + "..HEAD"
+	}
+
+	c, e := exec.Command("git", "rev-list", reference, "--count").Output()
 	if e != nil {
 		// TODO: handle error better
-		fmt.Println(e)
+		//log.Println(e)
 		return "0"
 	}
 
+	// Save caches
 	cacheCommitCount = strings.TrimSpace(string(c))
+	cacheCommitCountFromTagName = fromTag
+
 	return cacheCommitCount
 }
 
@@ -102,38 +88,39 @@ func getHEADHash(length int) string {
 	sha1 := strings.TrimSpace(string(c))
 	cacheHEADHash = sha1 // cache
 
+	if length > len(cacheHEADHash) {
+		length = len(cacheHEADHash)
+	}
 	return cacheHEADHash[:length]
 }
 
-func getDescription() (string, error) {
-	if cacheDescription != "" {
-		return cacheDescription, nil
+func getLastTag() (string, bool) {
+	if cacheLastTagName != "" {
+		return cacheLastTagName, true
 	}
 
-	vOut, verErr := exec.Command("git", "describe", "--tags", "--always").Output()
+	vOut, verErr := exec.Command("git", "describe", "--tags", "--abbrev=0").CombinedOutput()
 	if verErr != nil {
-		return "???", verErr
+		//log.Println(verErr)
+		return "", false
 	}
 
-	cacheDescription = strings.TrimSpace(string(vOut))
-	return cacheDescription, nil
+	tag := strings.TrimSpace(string(vOut))
+
+	// Has no tags
+	if tag == "" {
+		return tag, false
+	}
+
+	cacheLastTagName = tag
+	return cacheLastTagName, true
 }
 
-func getSHA1FromDescription(s string, hashLength int) string {
-	switch getTypeofDescription(s) {
-	case gitFullDescription:
-		ss := strings.Split(s, "-")
-		// safety measure
-		if len(ss) == 3 {
-			sha := ss[2]
-			if isHash(sha) {
-				return strings.TrimPrefix(sha, "g")
-			}
-		}
-	case gitHash:
-		return strings.TrimPrefix(s, "g")
-	}
-	return getHEADHash(hashLength)
+// Assumes using semver format for tags, eg v3.5.0 or 3.4.0
+func parseSemverFromTag(s string) []string {
+	tag := strings.TrimPrefix(s, "v")
+	vers := strings.Split(tag, ".")
+	return vers
 }
 
 // parseHashLength parses desired hash length output with default for none set
@@ -142,25 +129,58 @@ func getSHA1FromDescription(s string, hashLength int) string {
 // %S123 -> 123
 // %S -> defaultLen
 // NOTE: only compatible with single use #TODO?
-func parseHashLength(s string, defaultLen int) (int, error) {
-	re := regexp.MustCompile(`\%S(\d+)`)
+func parseHashLength(s string) (int, error) {
+	re := regexp.MustCompile(`%S(\d+)`)
 	m := re.MatchString(s)
 	// no digits following %S, use default
 	if !m {
-		return defaultLen, nil
+		return defaultHashLength, nil
 	}
 	f := re.FindAllString(s, 1)
 	if f == nil || len(f) == 0 {
-		return defaultLen, errors.New("regex return match but no matching string(s) found")
+		return 0, errors.New("regex return match but no matching string(s) found")
 	}
 	ff := f[0]
 	ff = strings.TrimPrefix(ff, "%S")
 	i, e := strconv.Atoi(ff)
 	if e != nil {
-		return defaultLen, e
+		return 0, e
 	}
 
 	return i, nil
+}
+
+// getB gets the semi-semver/mod patch number
+func getB() (string, bool) {
+	t, exists := getLastTag()
+	if !exists {
+		return "", false
+	}
+	semvers := parseSemverFromTag(t)
+	if len(semvers) != 3 {
+		return "", false
+	}
+	p := semvers[2]
+
+	c := getCommitCountFrom(t)
+
+	pi, e := strconv.Atoi(p)
+	if e != nil {
+		log.Println(e)
+		return "", false
+	}
+	pi = pi * 100
+
+	ci, e := strconv.Atoi(c)
+	if e != nil {
+		log.Println(e)
+		return "", false
+	}
+
+	bi := pi + ci
+	b := strconv.Itoa(bi)
+
+	return b, true
 }
 
 // GetVersion gets formatted git version
@@ -172,56 +192,94 @@ func parseHashLength(s string, defaultLen int) (int, error) {
 // %C, _C - commit count since last tag
 // %S, _S - HEAD sha1
 func GetVersion(format string) string {
+
+	var (
+		lastTag     string
+		commitCount string = "0"
+		semvers = []string{}
+		sha         string
+	)
+	semvers = nil
+
+	// Set default format.
 	if format == "" {
 		// v3.5.0+66-bbb06b1
 		format = "v%M.%m.%P-%S"
 	}
 
-	d, e := getDescription()
-	if e != nil {
-		fmt.Println(e)
-		return ""
-	}
-	out := format
+	// Need to get commit count
+	lastTag, _ = getTagIfTagOnHEADCommit()
+	// Is not 0
+	if lastTag == "" {
+		lastTag, _ = getLastTag()
+		// Either from init (entire branch) or lastTag
 
-	commitCount := getCommitCountFromDescription(d)
+	}
+
+	commitCount = getCommitCountFrom(lastTag)
+	if lastTag != "" {
+		semvers = parseSemverFromTag(lastTag)
+	}
 
 	// Convention alert:
 	// Want: when commit count is 0 (ie HEAD is on a tag), should yield only semver, eg v3.5.0
 	//       when commit count is >0 (ie HEAD is above a tag), should yield full "nightly" version name, eg v3.5.0+14-adfe123
 	// This syntax allows to signify tagged builds vs running builds.
+	// -- The point of this is just to be able to shift some logic out of CI scripts.
 	if format == "TAG_OR_NIGHTLY" {
-		out = "v%M.%m.%P+%C-%S"
+		format = "v%M.%m.%P+%C-%S"
 		if commitCount == "0" {
-			out = "v%M.%m.%P"
+			format = "v%M.%m.%P"
 		}
 	}
 
-	sha := getHEADHash(defaultHashLength)
+	sha = getHEADHash(defaultHashLength)
 	if strings.Index(format, "%S") >= 0 {
-		l, e := parseHashLength(format, defaultHashLength)
+		l, e := parseHashLength(format)
 		if e != nil {
 			log.Println(e)
 		}
-		sha = getSHA1FromDescription(d, l)
+		if l != defaultHashLength {
+			cacheHEADHash = ""
+			sha = getHEADHash(l)
+		}
 	}
 
-	semvers := getSemverFromDescription(d)
+	out := format
 
-	// -1 to replace indefinitely. Allows maximum user-decision-making.
-	out = strings.Replace(out, "%M", semvers[0], -1)
-	out = strings.Replace(out, "%m", semvers[1], -1)
-	out = strings.Replace(out, "%P", semvers[2], -1)
+	if semvers != nil {
+		// -1 to replace indefinitely. Allows maximum user-decision-making.
+		out = strings.Replace(out, "%M", semvers[0], -1)
+		out = strings.Replace(out, "_M", semvers[0], -1)
+		out = strings.Replace(out, "%m", semvers[1], -1)
+		out = strings.Replace(out, "_m", semvers[1], -1)
+		out = strings.Replace(out, "%P", semvers[2], -1)
+		out = strings.Replace(out, "_P", semvers[2], -1)
+	} else {
+		out = strings.Replace(out, "%M", "?", -1)
+		out = strings.Replace(out, "_M", "?", -1)
+		out = strings.Replace(out, "%m", "?", -1)
+		out = strings.Replace(out, "_m", "?", -1)
+		out = strings.Replace(out, "%P", "?", -1)
+		out = strings.Replace(out, "_P", "?", -1)
+	}
+
 	out = strings.Replace(out, "%C", commitCount, -1)
-	out = strings.Replace(out, "%S", sha, -1)
-
-	// Problem: escaping %'s in windows (ps or batch) is a pain in the ass.
-	// Solution: offer an extra set of escape vars for use with AppVeyor.
-	out = strings.Replace(out, "_M", semvers[0], -1)
-	out = strings.Replace(out, "_m", semvers[1], -1)
-	out = strings.Replace(out, "_P", semvers[2], -1)
 	out = strings.Replace(out, "_C", commitCount, -1)
-	out = strings.Replace(out, "_S", sha, -1)
+
+	re1 := regexp.MustCompile(`(%S(\d+|))`)
+	re2 := regexp.MustCompile(`(_S(\d+|))`)
+	out = re1.ReplaceAllLiteralString(out, sha)
+	out = re2.ReplaceAllLiteralString(out, sha)
+
+	b, ok := getB()
+	if ok {
+		out = strings.Replace(out, "%B", b, -1)
+		out = strings.Replace(out, "_B", b, -1)
+	} else {
+		out = strings.Replace(out, "%B", "?", -1)
+		out = strings.Replace(out, "_B", "?", -1)
+	}
 
 	return out
 }
--- gitvv/gitvv_test.go
+++ gitvv/gitvv_test.go
@@ -1,20 +1,20 @@
 package gitvv
 
 import (
+	"log"
+	"os"
 	"path/filepath"
 	"strings"
 	"testing"
-	"os"
-	"log"
 )
 
 const testDir = "testdata"
 
 var (
 	baseProjectDir string
-	noTagsDir string
-	onTagDir string
-	aboveTagDir string
+	noTagsDir      string
+	onTagDir       string
+	aboveTagDir    string
 )
 
 // Initialize multiple repo testdirs.
@@ -31,10 +31,88 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
+func Test_getLastTag(t *testing.T) {
+	table := []struct {
+		dir   string
+		wants string
+		wantb bool
+	}{
+		{noTagsDir, "", false},
+		{aboveTagDir, "v0.0.1", true},
+		{onTagDir, "v0.0.1", true},
+	}
+
+	for _, repo := range table {
+		if e := os.Chdir(repo.dir); e != nil {
+			t.Fatal(e)
+		}
+		// Print that we're in the right directory.
+		cwd, e := os.Getwd()
+		if e != nil {
+			t.Error(e)
+		}
+		t.Log(cwd)
+
+		// Clear cache
+		cacheLastTagName = ""
+
+		tag, ok := getLastTag()
+		if ok != repo.wantb {
+			t.Errorf("got: %v, want: %v", ok, repo.wantb)
+		}
+		if tag != repo.wants {
+			t.Errorf("got: %v, want: %v", tag, repo.wants)
+		}
+
+		if e := os.Chdir(baseProjectDir); e != nil {
+			t.Fatal(e)
+		}
+	}
+}
+
+func Test_getTagIfTagOnHEADCommit(t *testing.T) {
+	table := []struct {
+		dir   string
+		wants string
+		wantb bool
+	}{
+		{noTagsDir, "", false},
+		{aboveTagDir, "", false},
+		{onTagDir, "v0.0.1", true},
+	}
+
+	for _, repo := range table {
+		if e := os.Chdir(repo.dir); e != nil {
+			t.Fatal(e)
+		}
+		// Print that we're in the right directory.
+		cwd, e := os.Getwd()
+		if e != nil {
+			t.Error(e)
+		}
+		t.Log(cwd)
+
+		// Clear cache
+		cacheLastTagName = ""
+
+		tag, ok := getTagIfTagOnHEADCommit()
+		if ok != repo.wantb {
+			t.Errorf("got: %v, want: %v", ok, repo.wantb)
+		}
+		if tag != repo.wants {
+			t.Errorf("got: %v, want: %v", tag, repo.wants)
+		}
+
+		if e := os.Chdir(baseProjectDir); e != nil {
+			t.Fatal(e)
+		}
+	}
+}
+
 func Test_getHEADHash(t *testing.T) {
-	table := []struct{
-		dir string
-		want string
+	table := []struct {
+		dir        string
+		want       string
 		hashLength int
 	}{
 		{noTagsDir, "8673a80f120d8e11d607f1580da41c717e13863f", 7},
@@ -66,8 +144,157 @@ func Test_getHEADHash(t *testing.T) {
 			t.Error("unexpected: not a hash")
 		}
 
-		if h != repo.want {
+		if h != repo.want[:repo.hashLength] {
 			t.Errorf("unexpected hash: want: %s, got: %s", repo.want, h)
+		}
+
+		if e := os.Chdir(baseProjectDir); e != nil {
+			t.Fatal(e)
+		}
+	}
+}
+
+func Test_getCommitCountFrom(t *testing.T) {
+	table := []struct {
+		dir     string
+		fromTag string
+		want    string
+	}{
+		{noTagsDir, "", "1"},
+		{noTagsDir, "v9.9.9", "0"},
+		{aboveTagDir, "", "3"},
+		{aboveTagDir, "v0.0.1", "1"},
+		{onTagDir, "", "1"},
+		{onTagDir, "v0.0.1", "0"},
+	}
+
+	for _, repo := range table {
+		if e := os.Chdir(repo.dir); e != nil {
+			t.Fatal(e)
+		}
+		// Print that we're in the right directory.
+		cwd, e := os.Getwd()
+		if e != nil {
+			t.Error(e)
+		}
+		t.Log(cwd)
+
+		// Clear cache
+		cacheCommitCount = ""
+		cacheCommitCountFromTagName = ""
+
+		count := getCommitCountFrom(repo.fromTag)
+		if count != repo.want {
+			t.Errorf("got: %v, want: %v", count, repo.want)
+		}
+
+		if e := os.Chdir(baseProjectDir); e != nil {
+			t.Fatal(e)
+		}
+	}
+}
+
+func Test_getB(t *testing.T) {
+	table := []struct {
+		dir     string
+		wants    string
+		wantb bool
+	}{
+		{noTagsDir, "", false},
+		{aboveTagDir, "101", true},
+		{onTagDir, "100", true},
+	}
+
+	for _, repo := range table {
+		if e := os.Chdir(repo.dir); e != nil {
+			t.Fatal(e)
+		}
+		// Print that we're in the right directory.
+		cwd, e := os.Getwd()
+		if e != nil {
+			t.Error(e)
+		}
+		t.Log(cwd)
+
+		// Clear cache
+		cacheCommitCount = ""
+		cacheCommitCountFromTagName = ""
+		cacheLastTagName = ""
+
+		b, ok := getB()
+		if ok != repo.wantb {
+			t.Errorf("got: %v, want: %v", ok, repo.wantb)
+		}
+		if b != repo.wants {
+			t.Errorf("got: %v, want: %v", b, repo.wants)
+		}
+
+		if e := os.Chdir(baseProjectDir); e != nil {
+			t.Fatal(e)
+		}
+	}
+}
+
+func TestGetVersion(t *testing.T) {
+	table := []struct {
+		dir    string
+		format string
+		want   string
+	}{
+		{noTagsDir, "v", "v"},
+		{onTagDir, "v", "v"},
+		{aboveTagDir, "v", "v"},
+
+		{noTagsDir, "v%M.%m.%P", "v?.?.?"},
+		{onTagDir, "v%M.%m.%P", "v0.0.1"},
+		{aboveTagDir, "v%M.%m.%P", "v0.0.1"},
+
+		{noTagsDir, "%C", "1"},
+		{onTagDir, "%C", "0"},
+		{aboveTagDir, "%C", "1"},
+
+		{noTagsDir, "%S", "8673a80f120d8e11d607f1580da41c717e13863f"[:defaultHashLength]},
+		{onTagDir, "%S", "e35b683e9d2b32c444976484472980582b4c68a9"[:defaultHashLength]},
+		{aboveTagDir, "%S", "fe53b1e838d2fa761b3ce11d9fec683209f093a4"[:defaultHashLength]},
+
+		{noTagsDir, "%S4", "8673a80f120d8e11d607f1580da41c717e13863f"[:4]},
+		{onTagDir, "%S5", "e35b683e9d2b32c444976484472980582b4c68a9"[:5]},
+		{aboveTagDir, "%S6", "fe53b1e838d2fa761b3ce11d9fec683209f093a4"[:6]},
+
+		{noTagsDir, "v%M.%m.%P+%C-%S5", "v?.?.?+1-8673a"},
+		{onTagDir, "v%M.%m.%P+%C-%S5", "v0.0.1+0-e35b6"},
+		{aboveTagDir, "v%M.%m.%P+%C-%S5", "v0.0.1+1-fe53b"},
+
+		{noTagsDir, "TAG_OR_NIGHTLY", "v?.?.?+1-8673a80"},
+		{onTagDir, "TAG_OR_NIGHTLY", "v0.0.1"},
+		{aboveTagDir, "TAG_OR_NIGHTLY", "v0.0.1+1-fe53b1e"},
+
+		{noTagsDir, "v%M.%m.%B-%S5", "v?.?.?-8673a"},
+		{onTagDir, "v%M.%m.%B-%S5", "v0.0.100-e35b6"},
+		{aboveTagDir, "v%M.%m.%B-%S5", "v0.0.101-fe53b"},
+
+	}
+
+	for _, repo := range table {
+		if e := os.Chdir(repo.dir); e != nil {
+			t.Fatal(e)
+		}
+		// Print that we're in the right directory.
+		cwd, e := os.Getwd()
+		if e != nil {
+			t.Error(e)
+		}
+		t.Log(cwd)
+
+		// Clear cache
+		cacheHEADHash = ""
+		cacheCommitCount = ""
+		cacheLastTagName = ""
+		cacheCommitCountFromTagName = ""
+
+		got := GetVersion(repo.format)
+		if got != repo.want {
+			t.Errorf("got: %v, want: %v", got, repo.want)
 		}
 
 		if e := os.Chdir(baseProjectDir); e != nil {
@@ -98,35 +325,44 @@ func Test_isHash(t *testing.T) {
 	}
 }
 
-func Test_getCommitCountFromDescription(t *testing.T) {
-	table := []struct {
-		s    string
-		want string
-	}{
-		{"v0.1.7", "0"},
-		{"v3.5.0-64-g7658d85", "64"},
-	}
-
-	for _, tt := range table {
-		if got := getCommitCountFromDescription(tt.s); got != tt.want {
-			t.Errorf("hash: %s, got: %v, want: %v", tt.s, got, tt.want)
-		}
-	}
-}
-
-func Test_getSemverFromDescription(t *testing.T) {
+func Test_parseSemverFromTag(t *testing.T) {
 	table := []struct {
 		s    string
 		want []string
 	}{
 		{"v0.1.7", []string{"0", "1", "7"}},
-		{"v3.5.0-64-g7658d85", []string{"3", "5", "0"}},
-		{"dc1dfcc", []string{"0", "0", "0"}},
+		{"0.1.7", []string{"0", "1", "7"}},
 	}
 
 	for _, tt := range table {
-		if got := getSemverFromDescription(tt.s); strings.Join(got, "") != strings.Join(tt.want, "") {
-			t.Errorf("hash: %s, got: %v, want: %v", tt.s, got, tt.want)
+		if got := parseSemverFromTag(tt.s); strings.Join(got, "") != strings.Join(tt.want, "") {
+			t.Errorf("tag: %s, got: %v, want: %v", tt.s, got, tt.want)
+		}
+	}
+}
+
+func Test_parseHashLength(t *testing.T) {
+	table := []struct {
+		s     string
+		wantl int
+		wante error
+	}{
+		{"v%M.%m.%P+%C-%S", defaultHashLength, nil}, // test in big format string w/o specifier
+		{"v%M.%m.%P+%C-%S8", 8, nil},                // test in big format string with specifier
+		{"%S2", 2, nil},                             // test alone
+		{"v%S2", 2, nil},                            // test irrelevant prefix
+		{"%S109", 109, nil},                         // test not dependent on actual length of hash
+		{"%s109", defaultHashLength, nil},           // test not %S (no matching format)
+		{"%S17%S11", 17, nil},                       // test only works for single occurrence
+	}
+
+	for _, tt := range table {
+		i, e := parseHashLength(tt.s)
+		if e != tt.wante {
+			t.Fatalf("unexpected error: %v", e)
+		}
+		if i != tt.wantl {
+			t.Errorf("want: %v, got: %v", tt.wantl, i)
 		}
 	}
 }
